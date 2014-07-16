bdd221880f33e22696de481097ea9154362c5005
hakatashi <hakatasiloving@gmail.com>
2014-07-17 02:49:47 +0900

    Setup modules

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var morgan = require('morgan');
+var sqlite3 = require('sqlite3');
+var ejs = require('ejs');
+
 var app = express();
+app.use(morgan());
+
+app.get('/', function (req, res) {
+	res.send('It works!');
+});
 
 var server = app.listen(10721, function () {
 	console.log('Listening on port %d', server.address().port);
